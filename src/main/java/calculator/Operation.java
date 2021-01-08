a1e2305e8ed6a3925059ef13fcd0d3a62d227264
Tom Mens <tom.mens@umons.ac.be>
2021-01-08 20:32:59 +0100

    Simplified the code by removing the methods for infix, prefix and postfix by grouping them into a single method toString(Notation)
    Signed-off-by: Tom Mens <tom.mens@umons.ac.be>

--- src/main/java/calculator/Operation.java
+++ src/main/java/calculator/Operation.java
@@ -2,17 +2,19 @@ package calculator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public abstract class Operation implements Expression
 {  
   private List<Expression> args;
   protected String symbol;
   protected int neutral; // the neutral element of the operation (e.g. 1 for *, 0 for +)
-  public Notation notation = Notation.INFIX; //by default, printing uses infix notation
+  public Notation notation = Notation.INFIX; //by default, expressions are rendered as strings using infix notation
 
   // It is not allowed to create operation that have a null list of arguments.
   // Note that it is allowed to have an EMPTY list of arguments.
-  public /*constructor*/ Operation(List<Expression> elist) throws IllegalConstruction
+  public /*constructor*/ Operation(List<Expression> elist)
+		  throws IllegalConstruction
 	{ 
 	  if (elist == null) {
 		  throw new IllegalConstruction(); }
@@ -21,7 +23,8 @@ public abstract class Operation implements Expression
  	  }
     }
 
-  public /*constructor*/ Operation(List<Expression> elist,Notation n) throws IllegalConstruction
+  public /*constructor*/ Operation(List<Expression> elist,Notation n)
+		  throws IllegalConstruction
   {	
 	  if (elist == null) {
 		  throw new IllegalConstruction(); }
@@ -69,50 +72,29 @@ public abstract class Operation implements Expression
 			   .mapToInt(Expression::countNbs)
 			   .reduce(Integer::sum)
 			   .getAsInt();  
-}
+  }
 
-@Override
-final public String toString() {
-	switch (notation) {
-	case INFIX: {
-		return infix(); }
-	case PREFIX: {
-	    return prefix(); }
-	case POSTFIX: {
-		return postfix(); }
-	default: {
-		return "This case should never occur."; }
-	}
-}
+  @Override
+  final public String toString() {
+  	return toString(notation);
+  }
 
-  final public String infix() {
-	// use of Java 8 functional programming capabilities
-	String myString =
-			 args.stream()
-			     .map(Object::toString)
-			     .reduce((s1,s2) -> s1 + " " + symbol + " " + s2)
-			     .get();	
-	return "( " + myString + " )";
-    }
-
-  final public String prefix() {
-	// use of Java 8 functional programming capabilities
-	String myString =
-			 args.stream()
-			     .map(Object::toString)
-			     .reduce((s1,s2) -> s1 + ", " + s2)
-			     .get();	
-	return symbol + " (" + myString + ")";
-    }
-
-  final public String postfix() {
-	// use of Java 8 functional programming capabilities
-	String myString =
-			 args.stream()
-			     .map(Object::toString)
-			     .reduce((s1,s2) -> s1 + ", " + s2)
-			     .get();	
-	return "(" + myString + ") " + symbol;
-    }
+  final public String toString(Notation n) {
+   Stream<String> s = args.stream().map(Object::toString);
+   switch (notation) {
+	   case INFIX: return "( " +
+			              s.reduce((s1,s2) -> s1 + " " + symbol + " " + s2).get() +
+			              " )";
+	   case PREFIX: return symbol + " " +
+			               "(" +
+			               s.reduce((s1,s2) -> s1 + ", " + s2).get() +
+			               ")";
+	   case POSTFIX: return "(" +
+			                s.reduce((s1,s2) -> s1 + ", " + s2).get() +
+			                ")" +
+			                " " + symbol;
+	   default: return "This case should never occur.";
+	  }
+  }
 
 }
