87833f21571d099911c1068e792420e880d55181
Tom Mens <tom.mens@umons.ac.be>
2021-02-11 18:52:02 +0100

    Update versions in pom file to latest ones. (Works fine locally.)
    Signed-off-by: Tom Mens <tom.mens@umons.ac.be>

--- src/test/java/cucumbertests/RunCucumberTest.java
+++ src/test/java/cucumbertests/RunCucumberTest.java
@@ -5,6 +5,9 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber-report.html"})
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber-report.html"},
+        features = "src/test/resources"
+)
 public class RunCucumberTest {
 }
