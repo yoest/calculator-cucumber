21aaea10e7e22a78dd66e30bcfb6a979d25ccf31
Kreik2809 <200098@umons.ac.be>
2023-04-05 17:18:53 +0200

    Load fxml

--- src/main/java/gui/CalculatorGUI.java
+++ src/main/java/gui/CalculatorGUI.java
@@ -6,18 +6,20 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
+
+import java.io.File;
+
 public class CalculatorGUI extends Application {
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader((new File("src/main/resources/calculator.fxml" ).toURI().toURL()));
+        Scene scene = new Scene(loader.load());
         stage.setScene(scene);
+        stage.setTitle("Calculator");
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- src/main/java/gui/Run.java
+++
@@ -1,7 +0,0 @@
-package gui;
-
-public class Run {
-    public static void main(String[] args) {
-        CalculatorGUI.main(args);
-    }
-}
