373f8cf0c6e5a6bbc690f366d5f3798ffca159e2
Kreik2809 <200098@umons.ac.be>
2023-04-07 16:07:33 +0200

    Correct parser main template

--- src/main/java/calculatorParser/ParserMain.java
+++ src/main/java/calculatorParser/ParserMain.java
@@ -3,7 +3,13 @@ package calculatorParser;
 
 public class ParserMain {
     public static void main(String[] argv) throws Exception{
-        //pass
+        try {
+            parser p = new parser(new lexer(new java.io.StringReader("1+2*3")));
+            Object result = p.parse().value;
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
