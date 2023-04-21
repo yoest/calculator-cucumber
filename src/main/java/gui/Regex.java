27a2e08bc9ed49b2e9e57786247f61e3244c9989
Kreik2809 <200098@umons.ac.be>
2023-04-21 16:16:25 +0200

    Fix regex radix

--- src/main/java/gui/Regex.java
+++ src/main/java/gui/Regex.java
@@ -5,66 +5,74 @@ public class Regex {
     public static String DECIMAL = "[0-9\\|%$+\\-*/()\\s\\.]*";
 
     public static String TIME = "[0-9\\|.eEX%$+\\-*/()\\s:AMP]*";
-    public static String INTEGER_BASE_2 = "[01\\|%$+\\-/()\\s]*";
-    public static String INTEGER_BASE_3 = "[0-2\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_2 = "[01\\|%$+\\-/()\\s\\*]*";
+    public static String INTEGER_BASE_3 = "[0-2\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_4 = "[0-3\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_4 = "[0-3\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_5 = "[0-4\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_5 = "[0-4\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_6 = "[0-5\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_6 = "[0-5\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_7 = "[0-6\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_7 = "[0-6\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_8 = "[0-7\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_8 = "[0-7\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_9 = "[0-8\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_9 = "[0-8\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_10 = "[0-9\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_10 = "[0-9\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_11 = "[0-9Aa\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_11 = "[0-9Aa\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_12 = "[0-9A-Ba-b\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_12 = "[0-9A-Ba-b\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_13 = "[0-9A-Ca-c\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_13 = "[0-9A-Ca-c\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_14 = "[0-9A-Da-d\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_14 = "[0-9A-Da-d\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_15 = "[0-9A-Ea-e\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_15 = "[0-9A-Ea-e\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_16 = "[0-9A-Fa-f\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_16 = "[0-9A-Fa-f\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_17 = "[0-9A-Ga-g\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_17 = "[0-9A-Ga-g\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_18 = "[0-9A-Ha-h\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_18 = "[0-9A-Ha-h\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_19 = "[0-9A-Ia-i\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_19 = "[0-9A-Ia-i\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_20 = "[0-9A-Ja-j\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_20 = "[0-9A-Ja-j\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_21 = "[0-9A-Ka-k\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_21 = "[0-9A-Ka-k\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_22 = "[0-9A-La-l\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_22 = "[0-9A-La-l\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_23 = "[0-9A-Ma-m\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_23 = "[0-9A-Ma-m\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_24 = "[0-9A-Na-n\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_24 = "[0-9A-Na-n\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_25 = "[0-9A-Oa-o\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_25 = "[0-9A-Oa-o\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_26 = "[0-9A-Pa-p\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_26 = "[0-9A-Pa-p\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_27 = "[0-9A-Qa-q\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_27 = "[0-9A-Qa-q\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_28 = "[0-9A-Ra-r\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_28 = "[0-9A-Ra-r\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_29 = "[0-9A-Sa-s\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_29 = "[0-9A-Sa-s\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_30 = "[0-9A-Ta-t\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_30 = "[0-9A-Ta-t\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_31 = "[0-9A-Ua-u\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_31 = "[0-9A-Ua-u\\|%$+\\-/()\\s\\*]*";
 
-    public static String INTEGER_BASE_32 = "[0-9A-Va-v\\|%$+\\-/()\\s]*";
+    public static String INTEGER_BASE_32 = "[0-9A-Va-v\\|%$+\\-/()\\s\\*]*";
+
+    public  static  String INTEGER_BASE_33 = "[0-9A-Wa-w\\|%$+\\-/()\\s\\*]*";
+
+    public static String INTEGER_BASE_34 = "[0-9A-Xa-x\\|%$+\\-/()\\s\\*]*";
+
+    public static String INTEGER_BASE_35 = "[0-9A-Ya-y\\|%$+\\-/()\\s\\*]*";
+
+    public static String INTEGER_BASE_36 = "[0-9A-Za-z\\|%$+\\-/()\\s\\*]*";
 
     public static String getRegexInt(int base){
         switch(base){
@@ -130,6 +138,14 @@ public class Regex {
                 return INTEGER_BASE_31;
             case 32:
                 return INTEGER_BASE_32;
+            case 33:
+                return INTEGER_BASE_33;
+            case 34:
+                return INTEGER_BASE_34;
+            case 35:
+                return INTEGER_BASE_35;
+            case 36:
+                return INTEGER_BASE_36;
             default:
                 return null;
         }
